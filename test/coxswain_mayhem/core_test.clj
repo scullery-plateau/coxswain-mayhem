0cba41a989d5cb028a9a521413c3d955c38d3b1a
Voltron42 <dajohnson1978@gmail.com>
2022-09-17 22:27:47 -0500

    initial project setup

---
+++ test/coxswain_mayhem/core_test.clj
@@ -0,0 +1,7 @@
+(ns coxswain-mayhem.core-test
+  (:require [clojure.test :refer :all]
+            [coxswain-mayhem.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
